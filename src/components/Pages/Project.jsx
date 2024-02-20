dd556e5d40e6fdc0b028febe16e96c280f3e8997
shaunakahluwalia <shaunak.ahluwalia@paytm.com>
2024-02-20 19:16:29 +0530

    projects

--- src/components/Pages/Project.jsx
+++ src/components/Pages/Project.jsx
@@ -2,52 +2,46 @@ import React from 'react'
 import { ProjectCard } from '../ProjectCard'
 
 export const Project = ({theme}) => {
-  const project1 = {
-    "webUrl": "https://cinemate-shaunakk09.netlify.app",
-    "imageUrl": require("./icons/projects/Cinemate.png"),
-    "title": "Cinemate",
-    "subTitle": "Source for movie and TV content",
-  };
 
-  const project2 = {
-    "webUrl": "https://cinemate-shaunakk09.netlify.app",
-    "imageUrl": require("./icons/projects/TaskMate.png"),
-    "title": "TaskMate",
-    "subTitle": "Helps you manage your tasks",
-  };
-
-  const project3 = {
-    "webUrl": "https://github.com/Shaunakk09/sentiment_analysis",
-    "imageUrl": require("./icons/projects/Sentiment.png"),
-    "title": "Sentiment Analysis",
-    "subTitle": "Helps you understand setniment behind a review",
-  };
-
-  const project4 = {
-    "webUrl": "https://blackjack-shaunakk09.netlify.app/",
-    "imageUrl": require("./icons/projects/blackjack.png"),
-    "title": "Black Jack",
-    "subTitle": "Play black jack with computer",
-  };
-
-  const project5 = {
-    "webUrl": "https://codebook-shaunakk09.netlify.app/",
-    "imageUrl": require("./icons/projects/CodeBook.png"),
-    "title": "Code Book",
-    "subTitle": "E Commerce website for books",
-  };
-
+  const projects = [
+    {
+      "webUrl": "https://codebook-shaunakk09.netlify.app/",
+      "imageUrl": require("./icons/projects/CodeBook.png"),
+      "title": "Code Book",
+      "subTitle": "E Commerce website for books",
+    },
+    {
+      "webUrl": "https://cinemate-shaunakk09.netlify.app",
+      "imageUrl": require("./icons/projects/Cinemate.png"),
+      "title": "Cinemate",
+      "subTitle": "Source for movie and TV content",
+    },
+    {
+      "webUrl": "https://github.com/Shaunakk09/sentiment_analysis",
+      "imageUrl": require("./icons/projects/Sentiment.png"),
+      "title": "Sentiment Analysis",
+      "subTitle": "Helps you understand setniment behind a review",
+    },
+    {
+      "webUrl": "https://taskmate-shaunakk09.netlify.app",
+      "imageUrl": require("./icons/projects/TaskMate.png"),
+      "title": "TaskMate",
+      "subTitle": "Helps you manage your tasks",
+    },
+    {
+      "webUrl": "https://blackjack-shaunakk09.netlify.app/",
+      "imageUrl": require("./icons/projects/blackjack.png"),
+      "title": "Black Jack",
+      "subTitle": "Play black jack with computer",
+    },
+  ]
   return (
     <div>
         <main id="projects">
             <section className="py-2 sm:ml-20 sm:mb-20">
             <h5 className={`text-xl sm:text-2xl font-bold tracking-normal ${theme === "darkMode" ? "text-white" : "text-gray-900"} whitespace-nowrap`}>Projects</h5>
             <div className="flex flex-wrap items-center justify-center gap-y-4 sm:gap-y-8 gap-x-8 sm:gap-x-36">
-                <ProjectCard theme={theme} project={project1}/>
-                <ProjectCard theme={theme} project={project2}/>
-                <ProjectCard theme={theme} project={project3}/>
-                <ProjectCard theme={theme} project={project4}/>
-                <ProjectCard theme={theme} project={project5}/>
+                <ProjectCard theme={theme} projects={projects}/>
             </div>
             </section>
         </main>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import '../css/projectCard.css'
 
-export const ProjectCard = ({theme,project}) => {
+export const ProjectCard = ({theme,projects}) => {
   return (
-    <div className='flex flex-col'>
-        <h5 className={`mt-3 sm:mt-4 text-xs sm:text-base font-bold tracking-tight ${theme === "darkMode" ? "text-white" : "text-gray-900"} whitespace-nowrap`}>{project.title}</h5>
-        <div className={`mb-2 sm:mb-4 text-xs sm:text-base font-bold tracking-tight text-gray-400`}>{project.subTitle}</div>
-        <div className={`sm:mb-2 h-48 w-80 sm:h-52 max-w-full rounded-lg shadow`} 
-            style={{ 
-                backgroundSize: '100% 100%',
-                backgroundImage: `url(${project.imageUrl})`,
-                backgroundRepeat: 'no-repeat'
-            }}>
-            <img src={project.imageUrl} style={{display: 'none'}} alt="training" />
-        </div>
-    </div>
+    projects.map((project, index) => (
+      <div className='flex flex-col' key={index}>
+          <h5 className={`mt-3 sm:mt-4 text-xs sm:text-base font-bold tracking-tight ${theme === "darkMode" ? "text-white" : "text-gray-900"} whitespace-nowrap`}>{project.title}</h5>
+          <div className={`mb-2 sm:mb-4 text-xs sm:text-base font-bold tracking-tight text-gray-400`}>{project.subTitle}</div>
+          <div onClick={() => window.open(project.webUrl,"_blank")} className={`sm:mb-2 h-48 w-80 sm:h-52 max-w-full rounded-lg shadow transform transition-all duration-500 ease-in-out hover:scale-105 hover:opacity-80 image-container`} 
+              style={{ 
+                  backgroundSize: '100% 100%',
+                  backgroundImage: `url(${project.imageUrl})`,
+                  backgroundRepeat: 'no-repeat'
+              }}>
+              <img src={project.imageUrl} style={{display: 'none'}} alt="training" />
+              <img src='./icons/eye.svg' alt='eye icon' className='eye-icon' />
+          </div>
+      </div>
+    ))
   )
 }
